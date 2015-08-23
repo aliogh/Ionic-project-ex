b6eb7368eb6260f4b0e9e9c642817fe996f95b2a
JOSE MANUEL LOPEZ DOÑA <josema.lopez@extern.isban.com>
2015-08-24 00:15:22 +0200

    se añade pull-refresh al listado de playlist

--- src/client/app/features/playlists/playlists.controller.js
+++ src/client/app/features/playlists/playlists.controller.js
@@ -5,11 +5,27 @@
         .module('app.playlists')
         .controller('PlaylistsController', PlaylistsController);
 
-    function PlaylistsController(PlaylistsService) {
+    function PlaylistsController($scope, PlaylistsService) {
         var vm = this;
 
-        PlaylistsService.getPlaylists().then(function(data) {
-            vm.playlists = data;
-        });
+        activate();
+
+        function activate() {
+            PlaylistsService.getPlaylists()
+                .then(function(data) {
+                    vm.playlists = data;
+                });
+        }
+
+        $scope.doRefresh = function() {
+            PlaylistsService.getPlaylists()
+                .then(function(data) {
+                    vm.playlists = data;
+                })
+                .finally(function() {
+                    // Stop the ion-refresher from spinning
+                    $scope.$broadcast('scroll.refreshComplete');
+                });
+        };
     }
 })();
